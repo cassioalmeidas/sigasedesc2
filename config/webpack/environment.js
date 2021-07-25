058973316aac8ad46c959994bfe749d30338c978
Cassio Almeida <jcassiocosta@gmail.com>
2021-07-25 10:42:54 -0300

    configuração do webpacker melhorada

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,15 @@
-const { resolve } = require('path')
 const { environment, config } = require('@rails/webpacker')
 const webpack = require('webpack')
 const WebPackerPwa = require('webpacker-pwa')
 new WebPackerPwa(config, environment)
 
 environment.plugins.prepend('Provide',
-new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-})
+  new webpack.ProvidePlugin({
+      $: 'jquery/src/jquery',
+      jQuery: 'jquery/src/jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+  })
 )
 
 module.exports = environment
