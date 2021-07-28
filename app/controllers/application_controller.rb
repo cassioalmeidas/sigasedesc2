df8fbdf26983ba677d882ad68288cc4d91233ddf
Cassio Almeida <jcassiocosta@gmail.com>
2021-07-28 11:37:55 -0300

    páginas do devise, login, registro e senhas adaptadas de acordo com sb-admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   before_action :authenticate_usuario!
   self.responder = ApplicationResponder
+  layout :layout_by_resource
   respond_to :html
+
+  def layout_by_resource
+    devise_controller? ? 'devise' : 'application'
+  end
 end
