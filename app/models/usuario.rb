68b36177bc9b6c65d071669831f56c559806781a
Cassio Almeida <jcassiocosta@gmail.com>
2021-08-07 15:11:33 -0300

    melhoramentos no leiaute

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,6 +4,11 @@ class Usuario < ApplicationRecord
   validates :first_name, :last_name, :email, :password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+
+  def to_s
+    first_name.blank? && last_name.blank? ? email : "#{first_name} #{last_name}"
+  end
 end
