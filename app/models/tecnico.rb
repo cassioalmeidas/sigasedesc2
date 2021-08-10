6c735fd460b343f71eecd56022a8bfa73ad3e977
Cassio Almeida <jcassiocosta@gmail.com>
2021-08-10 09:59:47 -0300

    resolvido problemas com referencia da chave estrangeira de técnico em manutenção

--- app/models/tecnico.rb
+++ app/models/tecnico.rb
@@ -3,7 +3,7 @@ class Tecnico < ApplicationRecord
   has_paper_trail
   validates :nome, presence: true, uniqueness: true
   has_many :manutencao_tecnicos, dependent: :restrict_with_error
-  has_many :manutencoes_recebidas, class_name: 'Manutencao'
+  has_many :manutencoes_recebidas, dependent: :restrict_with_error, class_name: 'Manutencao', foreign_key: 'tecnico_recebeu_id'
   has_many :manutencoes_realizadas, through: :manutencao_tecnicos
 
   def to_s
